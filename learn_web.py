aabe4c47cd386be565821f106e06e7980a9bc9dd
Евгений Труфанов <evgenijtrufanov@MacBook-Pro-Evgenij.local>
2022-07-29 08:23:40 +0300

    new

--- learn_web.py
+++ learn_web.py
@@ -1,9 +1,22 @@
 import requests
+from bs4 import BeautifulSoup 
+query = 'cats'
+url = f'https://www.kiddle.co/s.php?q={query}'
+# url = 'https://kidkodschool.github.io/welcome.html'
 
-url = 'https://kidkodschool.github.io/welcome.html'
+page = requests.get(url).text
+soup = BeautifulSoup(page, 'html.parser')
+
 
 response = requests.get(url)
-with open("./python_is_cool.html", 'wb') as f:
-    f.write(response.content)
+# with open("./python_is_cool.html", 'wb') as f:
+#     f.write(response.content)
 
-
+for raw_img in soup.find_all('img'):
+    link = raw_img.get('src')
+    if link and link.startswith('https'):
+        response = requests.get(link)
+        with open("./today_avatar.jpg", 'wb') as f:
+            f.write(response.content)
+        break
+    
